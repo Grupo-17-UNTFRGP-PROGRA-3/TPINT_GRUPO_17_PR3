7ae541c64ee977cec85a2e7b1abeec6a09eff2e0
Elian Varela <elian_varela@outlook.com.ar>
2025-06-27 21:52:17 -0300

    Creación Login: validadores

--- Vistas/Administrador/Médicos/CreacionLogin.aspx.cs
+++ Vistas/Administrador/Médicos/CreacionLogin.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Clinica;
 
 namespace Vistas.Administrador.Médicos
 {
@@ -11,7 +12,50 @@ namespace Vistas.Administrador.Médicos
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
 
+            lblNombreMedico.Text = string.Empty;
+        }
+
+        protected void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtLegajo.Text = string.Empty;
+            txtUsuario.Text = string.Empty;
+            txtPass1.Text = string.Empty;
+            txtPass2.Text = string.Empty;
+        }
+        protected void BtnBuscarLegajo_Click(object sender, EventArgs e)
+        {
+            NegocioMedico negocioMedico = new NegocioMedico();
+            string legajo = txtLegajo.Text.ToString();
+
+            if (legajo != "0000" && negocioMedico.ExisteLegajo(legajo))
+            {
+                lblNombreMedico.Text = negocioMedico.ObtenerMedicoPorLegajo(legajo)._Nombre.ToString() + " " + negocioMedico.ObtenerMedicoPorLegajo(legajo)._Apellido.ToString();
+                pnlUsuarioMedico.Visible = true;
+                BtnBuscarLegajo.Visible = false;
+                BtnVolver2.Visible = false;
+            }
+            else
+            {
+                lblNombreMedico.Text = "No hay médico registrado con ese legajo";
+                pnlUsuarioMedico.Visible = false;
+                BtnBuscarLegajo.Visible = true;
+                BtnVolver2.Visible = true;
+            }
+        }
+
+        protected void txtLegajo_TextChanged(object sender, EventArgs e)
+        {
+            this.Page_Load(sender, e);
+            pnlUsuarioMedico.Visible = false;
+            BtnBuscarLegajo.Visible = true;
+        }
+
+        protected void btnVolver_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Administrador/Home.aspx");
         }
     }
+    
 }
